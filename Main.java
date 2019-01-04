8c684c818d8543679a0eb0c58dc844fdeaf3cca9
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-01-04 20:01:11 +0530

    Add PlusOne Pgm

--- Main.java
+++ Main.java
@@ -1,17 +1,23 @@
 package com.raghvendra;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int [] nums = {1, 7, 3, 6, 5, 6};
+        int [] nums = {9, 7, 8};
         //AtleastTwiceInArray
 //        AtleastTwiceInArray a = new AtleastTwiceInArray();
 //        int index = a.dominantIndex(nums);
 //        System.out.println("Dominant Index is : " + index);
         //PivotIndex
-        PivotIndex p = new PivotIndex();
-        int index = p.pivotIndex(nums);
-        System.out.println("Pivot Index is : " + index);
+//        PivotIndex p = new PivotIndex();
+//        int index = p.pivotIndex(nums);
+//        System.out.println("Pivot Index is : " + index);
+        //PlusOne
+        PlusOne p = new PlusOne();
+        int[] resultantArray = p.plusOne(nums);
+        System.out.println("ResultantArray: " + Arrays.toString(resultantArray));
     }
 }
