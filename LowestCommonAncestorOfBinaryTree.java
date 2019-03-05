7c338e5da2826d87bfb78fa922a42bd8ced256cf
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-03-05 02:28:47 -0800

    Add Lowest Common Ancestor in BST and Binary Tree, Remove Duplicates from Sorted List, Minimum Distance between BST Nodes

---
+++ LowestCommonAncestorOfBinaryTree.java
@@ -0,0 +1,32 @@
+package com.raghvendra;
+
+public class LowestCommonAncestorOfBinaryTree {
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x){
+            val = x;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) return null;
+        TreeNode result;
+        result = inOrder(root, p ,q);
+        return result;
+    }
+
+    private TreeNode inOrder(TreeNode root, TreeNode p, TreeNode q){
+        if(root != null){
+            if(root == p || root == q){
+                return root;
+            }
+            TreeNode left = inOrder(root.left, p, q);
+            TreeNode right = inOrder(root.right, p, q);
+            if(left != null && right != null) return root;
+            return (left != null) ? left : right;
+        }
+        return null;
+    }
+}
