994cc5538d7eb509a438f989559f71a2ada6b5e5
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-03-15 17:32:31 -0700

    Add Boundary of Binary Tree

--- BoundaryOfBinaryTree.java
+++ BoundaryOfBinaryTree.java
@@ -1,9 +1,7 @@
 package com.raghvendra;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Stack;
 
 public class BoundaryOfBinaryTree {
     private class TreeNode{
@@ -17,41 +15,56 @@ public class BoundaryOfBinaryTree {
 
     public List<Integer> boundaryOfBinaryTree(TreeNode root){
         List<Integer> result = new ArrayList<>();
-        List<Integer> nodeValues = new ArrayList<>();
-        Stack<Integer> storage =  new Stack<>();
-
         if(root == null) return result;
+        result.add(root.val);
+        result = travelLeftTreeWithoutLeaf(root.left, result);
+        result = travelLeafNodes(root.left, result);
+        result = travelLeafNodes(root.right, result);
+        result =  travelRightTreeWithoutLeaf(root.right, result);
+        return result;
+    }
 
-        LinkedList<TreeNode> current = new LinkedList<>();
-        LinkedList<TreeNode> next = new LinkedList<>();
+    private List<Integer> travelLeftTreeWithoutLeaf(TreeNode root, List<Integer> result){
 
-        current.add(root);
-
-        while(!current.isEmpty()){
-            TreeNode node = current.remove();
-
-            if(node.left != null){
-                next.add(node.left);
-            }
-            if(node.right !=null){
-                next.add(node.right);
-            }
-
-            nodeValues.add(node.val);
-
-            if(current.isEmpty()){
-                current = next;
-                next = new LinkedList<>();
-                if(!current.isEmpty()){
-                    result.add(nodeValues.get(0));
-                    if(nodeValues.size() !=1) storage.push(nodeValues.get(nodeValues.size()-1));
-                } else {
-                    for(Integer a : nodeValues) result.add(a);
-                }
-                nodeValues = new ArrayList<>();
-            }
+        //condition to check if the node is not leaf node and node is not null
+        if(root == null || (root.left == null && root.right == null)){
+            return result;
         }
-        while((storage.size()> 0) && storage.isEmpty()) result.add(storage.pop());
+        result.add(root.val);
+        if(root.left != null){
+            travelLeftTreeWithoutLeaf(root.left,result);
+        } else {
+            travelLeftTreeWithoutLeaf(root.right,result);
+        }
+        return result;
+    }
+
+    private List<Integer> travelLeafNodes(TreeNode root, List<Integer> result){
+
+        //
+        if(root != null){
+            //add the leaf nodes in the result list
+            if(root.left == null && root.right == null){
+                result.add(root.val);
+            }
+            travelLeafNodes(root.left, result);
+            travelLeafNodes(root.right, result);
+        }
+        return result;
+    }
+
+    private List<Integer> travelRightTreeWithoutLeaf(TreeNode root, List<Integer> result){
+
+        if(root == null || (root.left == null && root.right == null)){
+            return result;
+        }
+        if(root.right != null){
+            travelRightTreeWithoutLeaf(root.right,result);
+        } else{
+            travelRightTreeWithoutLeaf(root.left, result);
+        }
+        result.add(root.val);
+
         return result;
     }
 }
