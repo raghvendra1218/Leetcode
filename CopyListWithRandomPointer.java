46b4bbafcfa5e66c797a1355d691fe8a1551f0af
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-03-16 23:40:41 -0700

    Add Copy List with Random pointers

--- CopyListWithRandomPointer.java
+++ CopyListWithRandomPointer.java
@@ -13,7 +13,7 @@ public class CopyListWithRandomPointer {
         }
     }
 
-    public Node copyRandomList(Node head) {
+    /*public Node copyRandomList(Node head) {
         if(head == null) return null;
         Node curr = head;
         Node newHead = null;
@@ -64,6 +64,56 @@ public class CopyListWithRandomPointer {
         while(newCurr != null){
             newCurr.random = newCurr.random.random.next;
 //            curr = curr.next;
+            newCurr = newCurr.next;
+        }
+        return newHead;
+    }*/
+
+    /**
+     * Approach 2 : does not modifies the Original List, and that's the intention here
+     * Intuition : Add the duplicate nodes in between the original list and assign the random pointer to the next of the original random pointers
+     * Separate the Original and new Linked List
+     */
+
+    public Node copyRandomList(Node head) {
+        if(head == null) return head;
+        Node curr = head;
+        Node newHead = null;
+        Node fwd = null;
+
+        //iterate through the original list and add the new duplicate nodes in between with rand pointer as null for them
+        while(curr != null){
+            Node node = new Node(curr.val, null, null);
+            if(newHead == null){
+                newHead = node;
+            }
+            fwd = curr.next;
+            curr.next = node;
+            node.next = fwd;
+            curr = fwd;
+        }
+
+        //initialise the curr back to the head of the Original List
+        curr = head;
+
+        //iterate through the modified list and now assign the random pointers for inserted nodes
+        while(curr != null){
+            if(curr.random != null){
+                curr.next.random = curr.random.next;
+            } else {
+                curr.next.random = null;
+            }
+            curr = curr.next.next;
+        }
+
+        curr = head;
+        Node newCurr = newHead;
+
+        //Now separate the original and duplicate list
+        while(curr != null){
+            curr.next = curr.next.next;
+            newCurr.next = (newCurr.next != null) ? newCurr.next.next: null;
+            curr= curr.next;
             newCurr = newCurr.next;
         }
         return newHead;
