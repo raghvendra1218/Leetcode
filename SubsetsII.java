7c39846b0e7efd7ca617a9e7b74bd9f1f7bbef53
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-03-28 02:24:21 -0700

    Add Subsets II

---
+++ SubsetsII.java
@@ -0,0 +1,28 @@
+package com.raghvendra;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SubsetsII {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> tempList = new ArrayList<>();
+        //required in order the condition check which we are using nums[i] == nums[i - 1] to be valid
+        Arrays.sort(nums);
+        int start  = 0;
+        backTrack(result, tempList, start, nums);
+        return result;
+    }
+
+    private void backTrack(List<List<Integer>> result, List<Integer> tempList, int start, int[] nums){
+        result.add(new ArrayList<>(tempList));
+
+        for(int i = start; i < nums.length; ++i){
+            if(i > start && nums[i] == nums[i - 1]) continue;
+            tempList.add(nums[i]);
+            backTrack(result, tempList, i + 1, nums);
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+}
