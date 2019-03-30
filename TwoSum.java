a8bdc8eda52b27de0609b383f3d42300f2e4e909
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-03-30 09:55:41 -0700

    Modify Two Sum, Add One-pass Hash Table approach

--- TwoSum.java
+++ TwoSum.java
@@ -1,5 +1,18 @@
 package com.raghvendra;
 
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * Example:
+ *
+ * Given nums = [2, 7, 11, 15], target = 9,
+ *
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ */
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -56,6 +69,10 @@ public class TwoSum {
 
     /**
      * One Pass Solution, using HashMap
+     * Intuition :
+     * It turns out we can do it in one-pass. While we iterate and insert elements into the table,
+     * we also look back to check if current element's complement already exists in the table.
+     * If it exists, we have found a solution and return immediately.
      */
 
     public int[] twoSum(int[] nums, int target) {
