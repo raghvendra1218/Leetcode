10e0f97ee3988244f7d83d504525a6ef275b262a
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-03-31 01:55:04 -0700

    Modify Word Search

--- WordSearch.java
+++ WordSearch.java
@@ -1,5 +1,24 @@
 package com.raghvendra;
 
+/**
+ * Given a 2D board and a word, find if the word exists in the grid.
+ *
+ * The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring.
+ * The same letter cell may not be used more than once.
+ *
+ * Example:
+ *
+ * board =
+ * [
+ *   ['A','B','C','E'],
+ *   ['S','F','C','S'],
+ *   ['A','D','E','E']
+ * ]
+ *
+ * Given word = "ABCCED", return true.
+ * Given word = "SEE", return true.
+ * Given word = "ABCB", return false.
+ */
 public class WordSearch {
     public boolean exist(char[][] board, String word) {
         int row = 0;
