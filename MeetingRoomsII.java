142bcaaed64f528fef97032d7e36fe58f01ec9df
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-03-30 19:41:07 -0700

    Modify Meeting Rooms II

--- MeetingRoomsII.java
+++ MeetingRoomsII.java
@@ -1,5 +1,19 @@
 package com.raghvendra;
 
+/**
+ * Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei),
+ * find the minimum number of conference rooms required.
+ *
+ * Example 1:
+ *
+ * Input: [[0, 30],[5, 10],[15, 20]]
+ * Output: 2
+ * Example 2:
+ *
+ * Input: [[7,10],[2,4]]
+ * Output: 1
+ */
+
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
