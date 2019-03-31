dfcdb5281b8e0b4810d15e5850b1a1e2f3067751
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-03-31 01:56:52 -0700

    Modify Reverse Linked List

--- ReverseLinkedList.java
+++ ReverseLinkedList.java
@@ -1,5 +1,16 @@
 package com.raghvendra;
 
+/**
+ * Reverse a singly linked list.
+ *
+ * Example:
+ *
+ * Input: 1->2->3->4->5->NULL
+ * Output: 5->4->3->2->1->NULL
+ * Follow up:
+ *
+ * A linked list can be reversed either iteratively or recursively. Could you implement both?
+ */
 public class ReverseLinkedList {
 
     public class ListNode {
@@ -17,8 +28,9 @@ public class ReverseLinkedList {
             current.next = previous;
             previous = current;
             current = forward;
-            head = previous;
+//            head = previous;
         }
-        return head;
+//        return head;
+        return previous;
     }
 }
