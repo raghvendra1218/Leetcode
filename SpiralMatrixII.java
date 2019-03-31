932992bd9449d119edb948077691726cbd1197e5
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-03-30 20:56:29 -0700

    Modify Spiral Matrix II

--- SpiralMatrixII.java
+++ SpiralMatrixII.java
@@ -1,5 +1,19 @@
 package com.raghvendra;
 
+/**
+ * Given a positive integer n, generate a square matrix filled with elements from 1 to n2 in spiral order.
+ *
+ * Example:
+ *
+ * Input: 3
+ * Output:
+ * [
+ *  [ 1, 2, 3 ],
+ *  [ 8, 9, 4 ],
+ *  [ 7, 6, 5 ]
+ * ]
+ */
+
 public class SpiralMatrixII {
     public int[][] generateMatrix(int n) {
         int[][] result = new int[n][n];
