8a676b519f45566e3b97e270548cc24f168c9862
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-03-31 22:40:36 -0700

    Modify Longest Palindromic Substring

--- LongestPalindromicSubstring.java
+++ LongestPalindromicSubstring.java
@@ -1,6 +1,6 @@
 package com.raghvendra;
 
-public class LongestPalindromicSubstring {
+/*public class LongestPalindromicSubstring {
     public String longestPalindrome(String s) {
         if(s.length()== 0 || s.length() == 1) return s;
         boolean flag = true;
@@ -40,5 +40,36 @@ public class LongestPalindromicSubstring {
             j--;
         }
         return true;
+    }
+}*/
+
+/**
+ * Approach 2 : Time Complexity - O(n^2)
+ */
+
+public class LongestPalindromicSubstring {
+    public String longestPalindrome(String s) {
+        if(s.length() == 0) return "";
+        int left = 0, right = 0, maxLength = 0;
+        for(int index = 0; index < s.length(); ++index){
+            int len1 = checkPalindrome(s, index, index);
+            int len2 = checkPalindrome(s, index, index + 1);
+            int len = Math.max(len1, len2);
+            if(maxLength < len){
+                left = index - (len-1)/2;
+                right = index + (len/2);
+                maxLength = len;
+            }
+        }
+        return s.substring(left, right + 1);
+    }
+
+    public int checkPalindrome (String s, int left, int right){
+
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            left--;
+            right++;
+        }
+        return right - left - 1;
     }
 }
