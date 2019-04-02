e9265f134fda4ff044bd4e2237d1332d14a53982
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-04-02 15:17:56 -0700

    Add Decode ways

---
+++ DecodeWays.java
@@ -0,0 +1,72 @@
+package com.raghvendra;
+
+/**
+ * Solution 1 : Using Recursion
+ */
+public class DecodeWays {
+/*    public int numDecodings(String s) {
+        int k = s.length();
+        int result = 0;
+        result = helper(s, k, result);
+        return result;
+    }
+
+    private int helper(String s, int k, int result){
+        if( k == 0) return 1;
+
+        int firstLoc = s.length() - k;
+        if(s.charAt(firstLoc) == '0'){
+            return 0;
+        }
+        result = helper(s, k - 1, result);
+
+        //check the case where two letter digit should be less than 26, then there we need to add them as apart of combinations
+        if(k >= 2 && Integer.parseInt(s.substring(firstLoc, firstLoc + 2)) <= 26)
+        result += helper(s, k - 2, result);
+        return result;
+    }*/
+
+    /**
+     * Solution 2: Using Dynamic Programming
+     */
+    public int numDecodings(String s) {
+        int  k =  s.length();
+        int result = 0;
+        int[] memo  =  new int[k + 1];
+        for(int i = 0; i < memo.length; ++i){
+            memo[i] = -1;
+        }
+        result = helper(s, k, result, memo);
+        return result;
+    }
+
+    private int helper(String s, int k, int result, int[] memo){
+
+        //Base case, there is a one way if the string is empty to encode it
+        if(k == 0) return 1;
+
+        int firstLoc = s.length() - k;
+        //Handling the case when first digit in the string could be a zero, in that case there is no encoded string for that, so return zero
+        if(s.charAt(firstLoc) == '0'){
+            return 0;
+        }
+
+        //check if the result is stored in the memo array
+        if(memo[k] != -1){
+            return memo[k];
+        }
+
+        //calculate the result
+        result = helper(s, k - 1, result, memo);
+
+        //check the case where two letter digit should be less than 26, then there we need to add them as apart of combinations
+        if(k >= 2 && Integer.parseInt(s.substring(firstLoc, firstLoc + 2)) <= 26)
+        result += helper(s, k - 2, result, memo);
+
+        //store the value of result in memo
+        memo[k] = result;
+
+        return result;
+    }
+
+}
