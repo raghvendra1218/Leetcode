532ab50f14f0e224ec97b54a96b6c645f322d800
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-05-14 23:17:49 -0700

    Add Subarray Product Less Than K

--- SubarrayProductLessThanK.java
+++ SubarrayProductLessThanK.java
@@ -17,6 +17,14 @@ Note:
 0 <= k < 10^6.
 */
 
+/**
+ * Intuition :  Use Sliding window, and as we progress further we notice two things
+ * Number of Count for combinations has a pattern which is for each increment in end pointer the number of combinations less than k
+ * follow rule count += end - start + 1
+ * For each increase in end we check if the product is less than equal to k and if not then,
+ * dividing the product by nums[start] till the product >= k and we increment start
+ * At last return the count
+ */
 public class SubarrayProductLessThanK {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
         if(k <= 1)return 0;
