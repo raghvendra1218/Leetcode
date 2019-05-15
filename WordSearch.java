3c4465bb6645dcc91e6c49e1a42907d643547639
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-05-14 17:44:08 -0700

    Modify the Word Search description

--- WordSearch.java
+++ WordSearch.java
@@ -3,7 +3,8 @@ package com.raghvendra;
 /**
  * Given a 2D board and a word, find if the word exists in the grid.
  *
- * The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring.
+ * The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are
+ * those horizontally or vertically neighboring.
  * The same letter cell may not be used more than once.
  *
  * Example:
