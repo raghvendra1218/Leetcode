f264625bdb90243759a74f3138b9d6e5dbff4ff9
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-05-22 00:37:59 -0700

    Modify Three Sum

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 package com.raghvendra;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -325,8 +329,15 @@ public class Main {
 ////        System.out.println("Maximum Product of Three Numbers: "+ mp3nums.maximumProduct(nums));
 //        NextPermutation np = new NextPermutation();
 //        np.nextPermutation(nums);
-        String[] emails = {"test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"};
-        UniqueEmailAddresses uniqueEmailAddresses = new UniqueEmailAddresses();
-        System.out.println("Output: "+ uniqueEmailAddresses.numUniqueEmails(emails));
+//        String[] emails = {"test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"};
+//        UniqueEmailAddresses uniqueEmailAddresses = new UniqueEmailAddresses();
+//        System.out.println("Output: "+ uniqueEmailAddresses.numUniqueEmails(emails));
+        BotDemolition bd = new BotDemolition();
+        List<List<Integer>> lot =  new ArrayList<>();
+        lot.add(Arrays.asList(1,1,0));
+        lot.add(Arrays.asList(1,0,9));
+        lot.add(Arrays.asList(1,1,1));
+        System.out.println(lot);
+        System.out.println("Output: "+ bd.removeObstacle(3,3,lot));
      }
 }
