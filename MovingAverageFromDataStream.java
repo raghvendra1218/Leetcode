61754e75fa83cf157aa4824a1f8524eebeee4936
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2019-07-31 21:09:00 -0700

    Add Moving Average from Data Stream

--- MovingAverageFromDataStream.java
+++ MovingAverageFromDataStream.java
@@ -1,5 +1,18 @@
 package com.raghvendra;
 
+
+/**
+ * Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
+ *
+ * Example:
+ *
+ * MovingAverage m = new MovingAverage(3);
+ * m.next(1) = 1
+ * m.next(10) = (1 + 10) / 2
+ * m.next(3) = (1 + 10 + 3) / 3
+ * m.next(5) = (10 + 3 + 5) / 3
+ */
+
 public class MovingAverageFromDataStream {
     /** Initialize your data structure here. */
 /*     private Queue<Integer> myQueue;
