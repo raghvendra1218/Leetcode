f89fe2d732145ee0ceff56cfa697983fd5b69922
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2020-04-07 01:06:41 -0700

    High Five

---
+++ com/raghvendra/HighFive.java
@@ -0,0 +1,61 @@
+package com.raghvendra;
+
+/**
+ *
+ Given a list of scores of different students, return the average score of each student's top five scores in the order of each student's id.
+
+ Each entry items[i] has items[i][0] the student's id, and items[i][1] the student's score.  The average score is calculated using integer division.
+
+
+
+ Example 1:
+
+ Input: [[1,91],[1,92],[2,93],[2,97],[1,60],[2,77],[1,65],[1,87],[1,100],[2,100],[2,76]]
+ Output: [[1,87],[2,88]]
+ Explanation:
+ The average of the student with id = 1 is 87.
+ The average of the student with id = 2 is 88.6. But with integer division their average converts to 88.
+
+
+ Note:
+
+ 1 <= items.length <= 1000
+ items[i].length == 2
+ The IDs of the students is between 1 to 1000
+ The score of the students is between 1 to 100
+ For each student, there are at least 5 scores
+ */
+
+import java.util.*;
+
+public class HighFive {
+    public int[][] highFive(int[][] items) {
+        Map<Integer, ArrayList<Integer>> scoreMap = new HashMap<>();
+        for(int[] score: items) {
+            if(!scoreMap.containsKey(score[0])) scoreMap.put(score[0], new ArrayList(5));
+            if(scoreMap.get(score[0]).size() < 5) {
+                scoreMap.get(score[0]).add(score[1]);
+            } else {
+                Collections.sort(scoreMap.get(score[0]));
+                if(score[1] > scoreMap.get(score[0]).get(0)) {
+                    scoreMap.get(score[0]).remove(0);
+                    scoreMap.get(score[0]).add(score[1]);
+                }
+            }
+        }
+        int[][] result = new int [scoreMap.size()][2];
+        int idx = 0;
+        for(Map.Entry mapElement: scoreMap.entrySet()) {
+            int key = (int) mapElement.getKey();
+            List<Integer> listValue = (List<Integer>) mapElement.getValue();
+            int sum = 0;
+            for(int val : listValue) {
+                sum += val;
+            }
+            result[idx][0] = key;
+            result[idx][1] = sum/5;
+            idx++;
+        }
+        return result;
+    }
+}
