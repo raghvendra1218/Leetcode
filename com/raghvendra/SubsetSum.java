7704a25320105626975fa2d3482fb735f5cd0a08
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2020-04-26 22:22:44 -0700

    Subset Sum

--- com/raghvendra/SubsetSum.java
+++ com/raghvendra/SubsetSum.java
@@ -1,7 +1,13 @@
 package com.raghvendra;
 
 /**
- * Find whether the given sum could be obtained from a given array or not.
+ * Subset Sum Problem(Dynamic Programming)
+ * Given a set of non-negative integers, and a value sum, determine if there is a subset of the given set with sum equal to given sum.
+ * Example:
+ *
+ * Input:  set[] = {3, 34, 4, 12, 5, 2}, sum = 9
+ * Output:  True  //There is a subset (4, 5) with sum 9.
+ *
  * This is variant of Knapsack(0-1) problem
  */
 public class SubsetSum {
