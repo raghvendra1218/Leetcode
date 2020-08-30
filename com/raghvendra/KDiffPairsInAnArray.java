2870f6c4ace4254b0ff4c46238c43dc443d57953
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2020-08-30 00:06:09 -0700

    K Diff Pairs In An Array

---
+++ com/raghvendra/KDiffPairsInAnArray.java
@@ -0,0 +1,53 @@
+package com.raghvendra;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an array of integers and an integer k, you need to find the number of unique k-diff pairs in the array.
+ * Here a k-diff pair is defined as an integer pair (i, j), where i and j are both numbers in the array and their absolute difference is k.
+ *
+ * Example 1:
+ * Input: [3, 1, 4, 1, 5], k = 2
+ * Output: 2
+ * Explanation: There are two 2-diff pairs in the array, (1, 3) and (3, 5).
+ * Although we have two 1s in the input, we should only return the number of unique pairs.
+ * Example 2:
+ * Input:[1, 2, 3, 4, 5], k = 1
+ * Output: 4
+ * Explanation: There are four 1-diff pairs in the array, (1, 2), (2, 3), (3, 4) and (4, 5).
+ * Example 3:
+ * Input: [1, 3, 1, 5, 4], k = 0
+ * Output: 1
+ * Explanation: There is one 0-diff pair in the array, (1, 1).
+ * Note:
+ * The pairs (i, j) and (j, i) count as the same pair.
+ * The length of the array won't exceed 10,000.
+ * All the integers in the given input belong to the range: [-1e7, 1e7].
+ */
+
+public class KDiffPairsInAnArray {
+    public int findPairs(int[] nums, int k) {
+        int result = 0;
+        Map<Integer, Integer>map = new HashMap<>();
+
+        // traverse the nums in Hashmap keeping the key as the number and value as frequency of this number in nums array
+        for(int n : nums) {
+            map.put(n, map.getOrDefault(n, 0 ) + 1);
+        }
+
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()) {
+            int key = entry.getKey();
+            int val = entry.getValue();
+            //why k > 0 , because we don't want to include sets where we cannot form a pair because key + 0 contains in map will return true
+            //however this does not qualify as pair example [1,3,1,4,5] , k = 0  if we don't add condition k > 0 answer will be 4, but actual answer
+            //should be 1.
+            if(k > 0 && map.containsKey(k + key)) {
+                result++;
+            } else if (k == 0 && val > 1) {
+                result++;
+            }
+        }
+        return result;
+    }
+}
