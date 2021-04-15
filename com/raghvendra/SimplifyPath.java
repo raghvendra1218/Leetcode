4bd09102133a8f499e72dee0eec1590900f68313
raghvendra1218 <raghvendra.dixit@sjsu.edu>
2021-04-14 22:28:57 -0700

    SimplifyPath

---
+++ com/raghvendra/SimplifyPath.java
@@ -0,0 +1,18 @@
+package com.raghvendra;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class SimplifyPath {
+    public String simplifyPath(String path) {
+        String[] dirs = path.split("/");
+        Deque<String> stack = new ArrayDeque<>();
+        for(String s: dirs) {
+            if(s.equals("")) continue;
+            if(s.equals(".")) continue;
+            if(s.equals("..")) stack.pollLast();
+            else stack.add(s);
+        }
+        return "/" + String.join("/", stack);
+    }
+}
